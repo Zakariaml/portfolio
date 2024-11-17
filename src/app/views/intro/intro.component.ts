ec46856e882115f9d26eb75fc6ae868ea6589bbd
Zakariaml <zakaria.miloudi@outlook.com>
2024-11-17 14:30:01 +0100

    fixing route on prod & adding resume download

--- src/app/views/intro/intro.component.ts
+++ src/app/views/intro/intro.component.ts
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class IntroComponent {
   imageUrl = 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-WMKgR7u73KgyUS2JMY6LwL1gz9vWGR.png';
+
+  downloadResume() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/resume.pdf');
+    link.setAttribute('download', 'Zakaria_MILOUDI_Resume.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
